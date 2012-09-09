6b67a7eefae1c0cd440e3ce12797573a9b5908f4
Andreas Bruns <a.bruns@gmx.de>
2012-09-09 23:07:46 +0200

    get matchdays by webservice

--- src/de/bruns/botliga/thor10/CurrentMatchdayService.groovy
+++ src/de/bruns/botliga/thor10/CurrentMatchdayService.groovy
@@ -1,40 +1,47 @@
 package de.bruns.botliga.thor10
 
+@Grapes([
+   @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.2' ),
+   @Grab(group='net.sf.json-lib', module='json-lib', version='2.4', classifier='jdk15'),
+   @Grab(group='commons-beanutils', module='commons-beanutils', version='1.8.3')]
+)
+
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+
 class CurrentMatchdayService {
 
    def matches(round, year) {
       def allMatches = []
-      if (round == 1) {
-         allMatches.add(new Match(19702, 1, 7, 134))
-         allMatches.add(new Match(19703, 1, 87, 123))
-         allMatches.add(new Match(19706, 1, 112, 81))
-         allMatches.add(new Match(19707, 1, 95, 185))
-         allMatches.add(new Match(19708, 1, 100, 79))
-         allMatches.add(new Match(19709, 1, 115, 40))
-         allMatches.add(new Match(19710, 1, 91, 6))
-         allMatches.add(new Match(19704, 1, 16, 131))
-         allMatches.add(new Match(19705, 1, 55, 9))
-      } else if (round == 2) {
-         allMatches.add(new Match(19718, 2, 81, 115))
-         allMatches.add(new Match(19712, 2, 9, 95))
-         allMatches.add(new Match(19713, 2, 6, 112))
-         allMatches.add(new Match(19715, 2, 134, 100))
-         allMatches.add(new Match(19716, 2, 79, 7))
-         allMatches.add(new Match(19717, 2, 123, 91))
-         allMatches.add(new Match(19719, 2, 185, 87))
-         allMatches.add(new Match(19714, 2, 131, 55))
-         allMatches.add(new Match(19711, 2, 40, 16))
-      } else if (round == 3) {
-         allMatches.add(new Match(19726, 3, 95, 131))
-         allMatches.add(new Match(19720, 3, 7, 6))
-         allMatches.add(new Match(19721, 3, 40, 81))
-         allMatches.add(new Match(19722, 3, 87, 79))
-         allMatches.add(new Match(19723, 3, 16, 185))
-         allMatches.add(new Match(19724, 3, 55, 134))
-         allMatches.add(new Match(19727, 3, 115, 9))
-         allMatches.add(new Match(19725, 3, 112, 123))
-         allMatches.add(new Match(19728, 3, 91, 100))
+
+      def http = new HTTPBuilder( 'http://openligadb-json.heroku.com' )
+      
+      // perform a GET request, expecting JSON response data
+      http.request( GET, JSON ) {
+        uri.path = '/api/matchdata_by_group_league_saison'
+        uri.query = [ group_order_id: round, league_saison: year, league_shortcut: 'bl1' ]
+      
+        headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
+      
+        // response handler for a success response code:
+        response.success = { resp, json ->
+          // println json.matchdata[0].group_name + " -> " + json.matchdata[0].match_date_time
+      
+          // parse the JSON response object:
+          json.matchdata.each {
+            allMatches.add(new Match(it.match_id.toInteger(), round, it.id_team1.toInteger(), it.id_team2.toInteger()))
+            // println "  ${it.name_team1} - ${it.points_team1} : ${it.points_team2} - ${it.name_team2}"
+          }
+        }
+      
+        // handler for any failure status code:
+        response.failure = { resp ->
+          println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
+        }
       }
+      
       allMatches
    }
+
 }
